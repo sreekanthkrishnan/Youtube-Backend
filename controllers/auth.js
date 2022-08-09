85ac6bb2877ade31c87e488c4b9c35c6159d5b17
sreekanth <sreekanthkrishnan.talrop@gmail.com>
2022-08-09 17:43:09 +0530

    user signin and signup fuctionality cmpleted

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,9 @@
 import bcrypt from "bcryptjs";
+import { createError } from "../error.js";
 import User from "../models/User.js";
+import jwt from "jsonwebtoken";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     try {
         const salt = bcrypt.genSaltSync(10);
         const hash = bcrypt.hashSync(req.body.password, salt);
@@ -14,13 +16,49 @@ export const signup = async (req, res) => {
             status: "success",
         });
     } catch (err) {
-        // CREATE ERR HANDLING FUNCTION
-        console.log(err);
+        next(createError(err.status, err.message));
     }
 };
 
-export const signin = () => {
-    console.log("signin");
+export const signin = async (req, res, next) => {
+    try {
+        const user = await User.findOne({ name: req.body.name });
+        if (!user) return next(createError(404, "User not found"));
+
+        const isCorrect = await bcrypt.compare(
+            req.body.password,
+            user.password
+        );
+
+        if (!isCorrect) {
+            return next(createError(400, "Password don't match"));
+        } else {
+            const token = jwt.sign({ id: user._id }, process.env.JWT);
+            res.cookie("access_token", token, {
+                httpOnly: true,
+            }).json({
+                StatusCode: 6000,
+                status: "success",
+                access_token: token,
+                user: {
+                    id: user._id,
+                    name: user.name,
+                    email: user.email,
+                    subscribers: user.subscribers,
+                    subscribedUsers: user.subscribedUsers,
+                },
+            });
+        }
+
+        await newUser.save();
+
+        res.json({
+            StatusCode: 6000,
+            status: "success",
+        });
+    } catch (err) {
+        next(createError(err.status, err.message));
+    }
 };
 
 export const googleSignin = () => {
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-
+import cookieParser from "cookie-parser";
 // importing routes
 import authRoute from "./routes/auth.js";
 import userRoute from "./routes/users.js";
@@ -25,11 +25,22 @@ const connect = () => {
 };
 
 // ROUTES SETUP FUN
+
+app.use(cookieParser());
 app.use(express.json());
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/users", userRoute);
 app.use("/api/v1/videos", videoRoute);
 app.use("/api/v1/comments", commentRoute);
+
+app.use((err, req, res, next) => {
+    const status = err.status || 500;
+    const message = err.message || "Someting went wrong";
+    return res.status(status).json({
+        StatusCode: 6001,
+        message,
+    });
+});
 
 app.listen(8000, () => {
     connect();
