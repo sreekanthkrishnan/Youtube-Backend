479d621ab938c2741ce970cbcd9fb50af20f3bed
sreekanth <sreekanthkrishnan.talrop@gmail.com>
2022-08-14 19:14:13 +0530

    latest

---
+++ controllers/user.js
@@ -0,0 +1,21 @@
+export const update = (res, req, next) => {
+    return res.json("updated");
+};
+export const deleteUser = (res, req, next) => {
+    return res.json("updated");
+};
+export const getUser = (res, req, next) => {
+    return res.json("updated");
+};
+export const subscribe = (res, req, next) => {
+    return res.json("updated");
+};
+export const unSubscribe = (res, req, next) => {
+    return res.json("updated");
+};
+export const like = (res, req, next) => {
+    return res.json("updated");
+};
+export const unLike = (res, req, next) => {
+    return res.json("updated");
+};
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,35 @@
 import express from "express";
-import {} from "../controllers/user.js";
+import {
+    deleteUser,
+    getUser,
+    like,
+    subscribe,
+    unLike,
+    unSubscribe,
+    update,
+} from "../controllers/user.js";
 
 const router = express.Router();
+
+//UPDATE USER
+router.put("/:id", update);
+
+//DELETE A USER
+router.delete("/:id", deleteUser);
+
+//GET A USER
+router.get("/find/:id", getUser);
+
+//SUBSCRIBE A USER
+router.put("/subscribe/:id", subscribe);
+
+//UNSUBSCRIBE A USER
+router.put("/unsubscribe/:id", unSubscribe);
+
+//LIKE A VIDEO
+router.put("/like/:video_id", like);
+
+//DISLIKE A VIDEO
+router.put("/unlike/:video_id", unLike);
 
 export default router;
