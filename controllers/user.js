ed83e145761c5a794225ee491158932c3e4f1040
sreekanth <sreekanthkrishnan.talrop@gmail.com>
2022-08-15 16:10:58 +0530

    latets

--- controllers/user.js
+++ controllers/user.js
@@ -1,8 +1,29 @@
 import { createError } from "../error.js";
+import User from "../models/User.js";
 
-export const update = (req, res, next) => {
-    console.log(req.params, req.user);
+export const update = async (req, res, next) => {
+    console.log(req);
     if (req.params.id === req.user.id) {
+        try {
+            const updateUser = await User.findByIdAndUpdate(
+                req.params.id,
+                {
+                    $set: req.body,
+                },
+                { new: true }
+            );
+            res.status(200).json({
+                user: {
+                    id: updateUser._id,
+                    name: updateUser.name,
+                    email: updateUser.email,
+                    subscribers: updateUser.subscribers,
+                    subscribedUsers: updateUser.subscribedUsers,
+                },
+            });
+        } catch (err) {
+            next(err);
+        }
     } else {
         return next(createError(403, "Invaild access token"));
     }
